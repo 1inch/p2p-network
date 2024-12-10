9a3ecbb812779086b83890d361195125e3b4b285
Vitaly Vlasov <mail@vitv.ly>
2024-12-10 10:09:02 +0200

    Start cmd/internal integration; bufconn

--- resolver/rpc/rpc_server.go
+++ resolver/rpc/rpc_server.go
@@ -6,15 +6,11 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
-	"fmt"
-	"log"
 	"log/slog"
-	"net"
 	"os"
 
 	pb "github.com/1inch/p2p-network/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/test/bufconn"
 )
 
 type Config struct {
@@ -41,32 +37,6 @@ func generateKey() (*rsa.PrivateKey, error) {
 	}
 
 	return p, nil
-}
-
-func Start(c *Config) (*grpc.Server, error) {
-	var opts []grpc.ServerOption
-
-	grpcServer := grpc.NewServer(opts...)
-	server, err := newServer(c.LogLevel)
-	if err != nil {
-		return nil, err
-	}
-	pb.RegisterExecuteServer(grpcServer, server)
-
-	var lis net.Listener
-	if c.Testing {
-		slog.Info("### Create bufconn listener")
-		lis = bufconn.Listen(1024 * 1024)
-	} else {
-		lis, err = net.Listen("tcp", fmt.Sprintf("localhost:%d", c.Port))
-		if err != nil {
-			log.Fatalf("failed to listen: %v", err)
-		}
-	}
-	log.Printf("Listening on %d\n", c.Port)
-	go grpcServer.Serve(lis)
-
-	return grpcServer, nil
 }
 
 // NewRpcServer creates new RpcServer.
--- resolver/rpc/rpc_server_test.go
+++ resolver/rpc/rpc_server_test.go
@@ -3,32 +3,63 @@ package rpc
 import (
 	"context"
 	"log/slog"
+	"net"
 	"testing"
 
 	pb "github.com/1inch/p2p-network/proto"
 	"github.com/stretchr/testify/suite"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/test/bufconn"
 )
 
 type ResolverTestSuite struct {
 	suite.Suite
 
 	server *grpc.Server
+	client pb.ExecuteClient
+	conn   *grpc.ClientConn
 }
 
 func (s *ResolverTestSuite) SetupTest() {
-	server, err := Start(&Config{Port: 8001, LogLevel: slog.LevelInfo, Testing: false})
+	listener := bufconn.Listen(1024 * 1024)
+
+	var opts []grpc.ServerOption
+
+	grpcServer := grpc.NewServer(opts...)
+
+	server, err := newServer(slog.LevelInfo)
 	if err != nil {
-		slog.Info("###Failed to start server", "error", err)
+		slog.Error("newServer failed", "error", err)
 		return
 	}
+
+	pb.RegisterExecuteServer(grpcServer, server)
+
+	go func() {
+		if err := grpcServer.Serve(listener); err != nil {
+			s.Require().Fail("Failed to start grpc server")
+			return
+		}
+	}()
 	slog.Info("### Server started")
-	s.server = server
+	s.server = grpcServer
+
+	conn, err := grpc.DialContext(context.Background(), "", grpc.WithContextDialer(
+		func(context.Context, string) (net.Conn, error) {
+			return listener.Dial()
+		}), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		slog.Info("### error", "err", err)
+	}
+
+	s.conn = conn
+	s.client = pb.NewExecuteClient(conn)
 }
 
 func (s *ResolverTestSuite) TearDownTest() {
 	s.server.GracefulStop()
+	s.conn.Close()
 }
 
 func TestResolverTestSuite(t *testing.T) {
@@ -36,19 +67,15 @@ func TestResolverTestSuite(t *testing.T) {
 }
 
 func (s *ResolverTestSuite) TestExecute() {
-	conn, err := grpc.NewClient("localhost:8001", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		slog.Info("### error", "err", err)
-	}
 
-	client := pb.NewExecuteClient(conn)
-	s.Require().NotNil(client)
 	req := &pb.ResolverRequest{Id: "1", Payload: []byte("test"), Encrypted: false}
 
 	slog.Info("###about to execute")
-	var callOpts []grpc.CallOption = make([]grpc.CallOption, 0)
-	resp, err := client.Execute(context.Background(), req, callOpts...)
+	resp, err := s.client.Execute(context.Background(), req)
+	if err != nil {
+		slog.Info("Execute error", "error", err)
+		s.Require().Fail("execute error")
+	}
 	slog.Info("test output", "resp", resp)
 	s.Require().Equal(string(resp.Payload), "test")
-	defer conn.Close()
 }

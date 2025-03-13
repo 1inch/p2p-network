e134d386ae91200976ac2bab80a63b31e8f92ee3
shredd0r <andrii.vodianov@gmail.com>
2025-03-13 13:30:59 +0200

    add go-sdk

--- resolver/1inch_handler.go
+++ resolver/1inch_handler.go
@@ -2,27 +2,21 @@ package resolver
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"log/slog"
-	"net/http"
+	"strconv"
 
+	"github.com/1inch/1inch-sdk-go/sdk-clients/balances"
 	"github.com/1inch/p2p-network/resolver/types"
 )
 
-var errUnexpectedStatusCode = errors.New("unexpected status code")
-var errUnauthorizedStatusCode = errors.New("unauthorized call api")
+const apiUrl = "https://api.1inch.dev"
 
-const (
-	baseUrl                         = "https://api.1inch.dev/"
-	routingFormatToGetWalletBalance = "balance/v1.2/%s/balances/%s" // 1 - chainId, 2 - walletAddress
-)
-
+// oneInchApiHandler represends handler which would call 1inch api
 type oneInchApiHandler struct {
-	cfg    OneInchApiConfig
-	client *http.Client
-	logger *slog.Logger
+	cfg            OneInchApiConfig
+	logger         *slog.Logger
+	balanceClients map[string]*balances.Client // map<chainId, balances.Client>
 }
 
 // NewOneInchApiHandler creates an 1inch API handler instance
@@ -30,28 +24,64 @@ func NewOneInchApiHandler(cfg OneInchApiConfig, logger *slog.Logger) ApiHandler 
 	return &oneInchApiHandler{
 		cfg:    cfg,
 		logger: logger.WithGroup("1inch-handler-api"),
-		client: http.DefaultClient,
 	}
+}
+
+func (h *oneInchApiHandler) getClientByChainId(chainId string) (*balances.Client, error) {
+	if client, ok := h.balanceClients[chainId]; ok {
+		return client, nil
+	}
+
+	chainIdInt, err := strconv.ParseUint(chainId, 10, 64)
+	if err != nil {
+		h.logger.Error("Invalid chainId format", slog.Any("chainId", chainId))
+		return nil, err
+	}
+
+	config, err := balances.NewConfiguration(
+		balances.ConfigurationParams{
+			ChainId: chainIdInt,
+			ApiUrl:  apiUrl,
+			ApiKey:  h.cfg.Key,
+		},
+	)
+
+	if err != nil {
+		h.logger.Error(fmt.Sprintf("Failed create configuration for client of balances for chainId: %s", chainId), slog.Any("err", err.Error()))
+		return nil, err
+	}
+
+	newClient, err := balances.NewClient(config)
+
+	if err != nil {
+		h.logger.Error(fmt.Sprintf("Failed create client of balances for chainId: %s", chainId), slog.Any("err", err.Error()))
+		return nil, err
+	}
+
+	h.balanceClients[chainId] = newClient
+
+	return newClient, nil
 }
 
 // Process acts as an API wrapper for JSON payloads coming through gRPC
 func (h *oneInchApiHandler) Process(req *types.JsonRequest) (*types.JsonResponse, error) {
+
 	switch req.Method {
 	case "GetWalletBalance":
-		balance, err := h.getWalletBalance(req.Params)
+		balances, err := h.getWalletBalance(req.Params)
 		if err != nil {
 			return &types.JsonResponse{Id: req.Id, Result: 0}, err
 		}
-		return &types.JsonResponse{Id: req.Id, Result: balance}, nil
+		return &types.JsonResponse{Id: req.Id, Result: balances}, nil
 	default:
 		return &types.JsonResponse{Id: req.Id, Result: 0}, errUnrecognizedMethod
 	}
 }
 
-func (h *oneInchApiHandler) getWalletBalance(params []string) (string, error) {
+func (h *oneInchApiHandler) getWalletBalance(params []string) (interface{}, error) {
 	if len(params) != 2 {
 		h.logger.Error("GetWalletBalance: wrong number of params", "len", len(params))
-		return "", errWrongParamCount
+		return nil, errWrongParamCount
 	}
 	chainId := params[0]
 	address := params[1]
@@ -59,36 +89,28 @@ func (h *oneInchApiHandler) getWalletBalance(params []string) (string, error) {
 	err := h.validateRequest(chainId, address)
 	if err != nil {
 		h.logger.Error("failed validate request for GetWalletBalance")
-		return "", err
-	}
-	request, err := h.formatHttpRequest(chainId, address)
-	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	resp, err := h.client.Do(request)
-	defer func() {
-		err := resp.Body.Close()
-		if err != nil {
-			h.logger.Error("failed close response bodys")
-		}
-	}()
+	client, err := h.getClientByChainId(chainId)
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.GetBalancesByWalletAddress(
+		context.Background(),
+		balances.BalancesByWalletAddressParams{
+			Wallet: address,
+		},
+	)
+
 	if err != nil {
 		h.logger.Error("failed invoking JSON-RPC request", slog.Any("err", err.Error()))
-		return "", err
+		return nil, err
 	}
 
-	if resp.StatusCode == http.StatusUnauthorized {
-		h.logger.Error("problems with authorization token, returned unauthorized status code")
-		return "", errUnauthorizedStatusCode
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		h.logger.Error("returned invalid status code from 1inch", slog.Any("status", resp.Status))
-		return "", errUnexpectedStatusCode
-	}
-
-	return h.decodeBalance(resp)
+	return resp, nil
 }
 
 func (h *oneInchApiHandler) validateRequest(chainId, address string) error {
@@ -101,34 +123,4 @@ func (h *oneInchApiHandler) validateRequest(chainId, address string) error {
 	}
 
 	return nil
-}
-
-func (h *oneInchApiHandler) formatHttpRequest(chainId, address string) (*http.Request, error) {
-	requestUrl := baseUrl + fmt.Sprintf(routingFormatToGetWalletBalance, chainId, address)
-	request, err := http.NewRequestWithContext(context.Background(), http.MethodGet, requestUrl, nil)
-
-	if err != nil {
-		h.logger.Error("failed create http request", slog.Any("err", err.Error()))
-		return nil, err
-	}
-
-	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", h.cfg.Key))
-	request.Header.Add("Content-Type", "application/json")
-
-	return request, nil
-}
-
-func (h *oneInchApiHandler) decodeBalance(resp *http.Response) (string, error) {
-	var balance string
-
-	decoder := json.NewDecoder(resp.Body)
-	err := decoder.Decode(&balance)
-
-	if err != nil {
-		h.logger.Error("failed decode wallet balance", slog.Any("err", err.Error()))
-		return "", err
-	}
-
-	h.logger.Info("GetWalletBalance() processed")
-	return balance, nil
 }
